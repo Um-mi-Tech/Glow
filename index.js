532baa781caa622748af39be70a093ef78eebf8a
Um-mi-Tech <Ummuhlikuthum@gmail.com>
2024-11-13 02:12:14 -0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 let shoppingCart = 0;
 
-const swiper = new Swiper('.card-wrapper', {
+var swiper = new Swiper('.card-wrapper',{
+  loop: true,
+  spaceBetween:30,
+
+//paigation bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:true,
+    dynamicBullets:true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints:{
+   0: {
+     slidesPerView:1  
+   },
+   768:{
+    slidesPerView:2  
+   },
+   1024:{
+    slidesPerView:4 
+   },
+  }
+});
+
+
+
+var swiperTwo = new Swiper('.Blog-wrapper',{
   loop: true,
   spaceBetween:30,
 
